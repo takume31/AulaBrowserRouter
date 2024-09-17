be52d14642025694ddea83c2366daee533981cfb
takume31 <henrique.nakamura@estudante.ifms.edu.br>
2024-09-17 16:07:22 -0400

    abu

--- src/Paginaini/index.jsx
+++ src/Paginaini/index.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
+import Footer from "../Footer";
 import { Link } from "react-router-dom"
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import Header from "../Header";
-import Footer from "../Footer";
 
 export default function Produtos(){
+    const [gmail, setGmail] = useState(""); 
+    const [senha, setSenha] = useState("");
+
+  const [Listasalva, setlistasalva] = useState([]);
+    const salvarcontatos = (event) => {
+        event.preventDefault();
+        setlistasalva([...Listasalva, 
+            {
+                Gmailsalvo: gmail,
+                Senhasalvo: senha
+            }
+        ]);
+    }
+    console.table = Listasalva
   
-
-
     return(
         <>
         <Header/>
@@ -35,6 +48,37 @@ export default function Produtos(){
                     </div>
             </Carousel>
             </div>  
+            </div>
+            <br/>
+            <div className="cada">
+                <div className="ite">
+                    
+            <label htmlFor="gma">Gmail:  </label>
+                <input type="gma" name="" id="" onChange={(event)=> setGmail(event.target.value)}/>
+                <br/>
+                {gmail}
+            <br/>
+            <br/>
+            
+            <label htmlFor="sen">Senha:  </label>
+            <input type="sen" name="" id="" onChange={(event)=> setSenha(event.target.value)}/>
+            <br/>
+                {senha}
+            <br/>
+            <br/>
+            <center>
+            <button onClick={salvarcontatos}>SALVAR</button>
+            </center>
+            {
+                Listasalva.map((contato, index) =>
+            <div key={index}>
+                <div className="lista">
+                <p>Gmail: {contato.Gmailsalvo}</p>
+                <p>senha: {contato.Senhasalvo}</p>
+                </div>
+                </div>
+            )}
+            </div>
             </div>
         </div>
         <br/>
